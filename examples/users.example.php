f6d9b9ca3f9b40a13065482c294f098b1d41f29b
Nicklas Hintze <nicklas.hintze@theservicecorporation.com>
2023-06-09 23:19:04 +0200

    modified user.example

--- examples/users.example.php
+++ examples/users.example.php
@@ -5,9 +5,12 @@ use Nichin79\Zendesk\Zendesk;
 
 $zendesk = new Zendesk($config['zendesk']);
 // $request = $zendesk->users();
+$request = $zendesk->tickets->show(6199);
 
-$request = $zendesk->users->me();
+// $request = $zendesk->users->me();
 
 // echo json_encode($request->response());
-echo json_encode($request->response(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+echo json_encode($request->response());
+// echo json_encode($request->response(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+echo "\r\n";
 echo $request->httpcode();
